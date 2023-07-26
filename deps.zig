c985740cf5d886587033cc6bc2fd6c0c74118ca3
Jeff <jeff@jeffreydvp.com>
2023-07-26 12:54:48 -0600

    Zigmod updates

--- deps.zig
+++ deps.zig
@@ -1,78 +1,13 @@
 // zig fmt: off
 const std = @import("std");
 const builtin = @import("builtin");
-const ModuleDependency = std.build.ModuleDependency;
 const string = []const u8;
+const ModuleDependency = std.build.ModuleDependency;
 
-pub const GitExactStep = struct {
-    step: std.build.Step,
-    builder: *std.build.Builder,
-    url: string,
-    commit: string,
-
-        pub fn create(b: *std.build.Builder, url: string, commit: string) *GitExactStep {
-            var result = b.allocator.create(GitExactStep) catch @panic("memory");
-            result.* = GitExactStep{
-                .step = std.build.Step.init(.{
-                    .id = .custom,
-                    .name = b.fmt("git clone {s} @ {s}", .{ url, commit }),
-                    .owner = b,
-                    .makeFn = make,
-                }),
-                .builder = b,
-                .url = url,
-                .commit = commit,
-            };
-
-            var urlpath = url;
-            urlpath = trimPrefix(u8, urlpath, "https://");
-            urlpath = trimPrefix(u8, urlpath, "git://");
-            const repopath = b.fmt("{s}/zigmod/deps/git/{s}/{s}", .{ b.cache_root.path.?, urlpath, commit });
-            flip(std.fs.cwd().access(repopath, .{})) catch return result;
-
-            var clonestep = std.build.RunStep.create(b, "clone");
-            clonestep.addArgs(&.{ "git", "clone", "-q", "--progress", url, repopath });
-            result.step.dependOn(&clonestep.step);
-
-            var checkoutstep = std.build.RunStep.create(b, "checkout");
-            checkoutstep.addArgs(&.{ "git", "-C", repopath, "checkout", "-q", commit });
-            result.step.dependOn(&checkoutstep.step);
-            checkoutstep.step.dependOn(&clonestep.step);
-
-            return result;
-        }
-
-        fn make(step: *std.build.Step, prog_node: *std.Progress.Node) !void {
-            _ = step;
-            _ = prog_node;
-        }
-};
-
-pub fn fetch(exe: *std.build.LibExeObjStep) void {
-    const b = exe.step.owner;
-    inline for (comptime std.meta.declarations(package_data)) |decl| {
-        const path = &@field(package_data, decl.name).entry;
-        const root = if (@field(package_data, decl.name).store) |_| b.cache_root.path.? else ".";
-        if (path.* != null) path.* = b.fmt("{s}/zigmod/deps{s}", .{ root, path.*.? });
-    }
-    exe.step.dependOn(&GitExactStep.create(b, "https://github.com/jecolon/ziglyph.git", "1201bf393a9c8ae4e1564d01e4e4d9377e2c89de").step);
-}
-
-fn trimPrefix(comptime T: type, haystack: []const T, needle: []const T) []const T {
-    if (std.mem.startsWith(T, haystack, needle)) {
-        return haystack[needle.len .. haystack.len];
-    }
-    return haystack;
-}
-
-fn flip(foo: anytype) !void {
-    _ = foo catch return;
-    return error.ExpectedError;
-}
+pub const cache = ".zigmod/deps";
 
 pub fn addAllTo(exe: *std.build.LibExeObjStep) void {
     checkMinZig(builtin.zig_version, exe);
-    fetch(exe);
     const b = exe.step.owner;
     @setEvalBranchQuota(1_000_000);
     for (packages) |pkg| {
@@ -83,22 +18,21 @@ pub fn addAllTo(exe: *std.build.LibExeObjStep) void {
     var vcpkg = false;
     inline for (comptime std.meta.declarations(package_data)) |decl| {
         const pkg = @as(Package, @field(package_data, decl.name));
-        const root = if (pkg.store) |st| b.fmt("{s}/zigmod/deps/{s}", .{ b.cache_root.path.?, st }) else ".";
         for (pkg.system_libs) |item| {
             exe.linkSystemLibrary(item);
             llc = true;
         }
         for (pkg.frameworks) |item| {
-            if (!builtin.target.isDarwin()) @panic(exe.step.owner.fmt("a dependency is attempting to link to the framework {s}, which is only possible under Darwin", .{item}));
+            if (!builtin.target.isDarwin()) @panic(b.fmt("a dependency is attempting to link to the framework {s}, which is only possible under Darwin", .{item}));
             exe.linkFramework(item);
             llc = true;
         }
         for (pkg.c_include_dirs) |item| {
-            exe.addIncludePath(b.fmt("{s}/{s}", .{ root, item }));
+            exe.addIncludePath(b.fmt("{s}/{s}", .{ @field(dirs, decl.name), item }));
             llc = true;
         }
         for (pkg.c_source_files) |item| {
-            exe.addCSourceFile(b.fmt("{s}/{s}", .{ root, item }), pkg.c_source_flags);
+            exe.addCSourceFile(b.fmt("{s}/{s}", .{ @field(dirs, decl.name), item }), pkg.c_source_flags);
             llc = true;
         }
         vcpkg = vcpkg or pkg.vcpkg;
@@ -108,10 +42,8 @@ pub fn addAllTo(exe: *std.build.LibExeObjStep) void {
 }
 
 pub const Package = struct {
-    name: string = "",
-    entry: ?string = null,
-    store: ?string = null,
-    deps: []const *Package = &.{},
+    directory: string,
+    pkg: ?Pkg = null,
     c_include_dirs: []const string = &.{},
     c_source_files: []const string = &.{},
     c_source_flags: []const string = &.{},
@@ -122,17 +54,18 @@ pub const Package = struct {
 
     pub fn zp(self: *Package, b: *std.build.Builder) ModuleDependency {
         var temp: [100]ModuleDependency = undefined;
-        for (self.deps, 0..) |item, i| {
+        const pkg = self.pkg.?;
+        for (pkg.dependencies, 0..) |item, i| {
             temp[i] = item.zp(b);
         }
         if (self.module) |mod| {
             return mod;
         }
         const result = ModuleDependency{
-            .name = self.name,
+            .name = pkg.name,
             .module = b.createModule(.{
-                .source_file = .{ .path = self.entry.? },
-                .dependencies = b.allocator.dupe(ModuleDependency, temp[0..self.deps.len]) catch @panic("oom"),
+                .source_file = pkg.source,
+                .dependencies = b.allocator.dupe(ModuleDependency, temp[0..pkg.dependencies.len]) catch @panic("oom"),
             }),
         };
         self.module = result;
@@ -140,33 +73,52 @@ pub const Package = struct {
     }
 };
 
+pub const Pkg = struct {
+    name: string,
+    source: std.build.FileSource,
+    dependencies: []const *Package,
+};
+
 fn checkMinZig(current: std.SemanticVersion, exe: *std.build.LibExeObjStep) void {
     const min = std.SemanticVersion.parse("null") catch return;
     if (current.order(min).compare(.lt)) @panic(exe.step.owner.fmt("Your Zig version v{} does not meet the minimum build requirement of v{}", .{current, min}));
 }
 
+pub const dirs = struct {
+    pub const _root = "";
+    pub const _i1yufq9oyy49 = cache ++ "/../..";
+    pub const _l21900j9ct9e = cache ++ "/git/github.com/jecolon/ziglyph";
+    pub const _r5hwws1i0u5g = cache ++ "/git/github.com/jeffbdavenport/SDL.zig";
+};
+
 pub const package_data = struct {
     pub var _i1yufq9oyy49 = Package{
-        .name = "dungeon_weld",
-        .entry = "/../..//src/main.zig",
+        .directory = dirs._i1yufq9oyy49,
+        .pkg = Pkg{ .name = "dungeon_weld", .source = .{ .path = dirs._i1yufq9oyy49 ++ "/src/main.zig" }, .dependencies = &.{} },
     };
     pub var _l21900j9ct9e = Package{
-        .store = "/git/github.com/jecolon/ziglyph/1201bf393a9c8ae4e1564d01e4e4d9377e2c89de",
-        .name = "ziglyph",
-        .entry = "/git/github.com/jecolon/ziglyph/1201bf393a9c8ae4e1564d01e4e4d9377e2c89de/src/ziglyph.zig",
+        .directory = dirs._l21900j9ct9e,
+        .pkg = Pkg{ .name = "ziglyph", .source = .{ .path = dirs._l21900j9ct9e ++ "/src/ziglyph.zig" }, .dependencies = &.{} },
+    };
+    pub var _r5hwws1i0u5g = Package{
+        .directory = dirs._r5hwws1i0u5g,
+        .pkg = Pkg{ .name = "SDL.zig", .source = .{ .path = dirs._r5hwws1i0u5g ++ "/src/sdl.zig" }, .dependencies = &.{} },
     };
     pub var _root = Package{
+        .directory = dirs._root,
     };
 };
 
-pub const packages = [_]*Package{
+pub const packages = &[_]*Package{
     &package_data._i1yufq9oyy49,
     &package_data._l21900j9ct9e,
+    &package_data._r5hwws1i0u5g,
 };
 
 pub const pkgs = struct {
     pub const dungeon_weld = &package_data._i1yufq9oyy49;
     pub const ziglyph = &package_data._l21900j9ct9e;
+    pub const SDL_zig = &package_data._r5hwws1i0u5g;
 };
 
 pub const imports = struct {
